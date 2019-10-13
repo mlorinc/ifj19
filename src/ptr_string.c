5ccfdf002465aca03407b9fe0509f3e0e1984572
Patrik Ondriga <patrik@localhost.localdomain>
2019-10-13 19:37:44 +0200

    add functionality for ptr_string_new_with_length and ptr_string_clone

---
+++ src/ptr_string.c
@@ -0,0 +1,27 @@
+#include "ptr_string.h"
+
+ptr_string_t* ptr_string_new_with_length(const size_t initial_length)
+{
+    ptr_string_t* new_ptr = (ptr_string_t*) malloc(sizeof(ptr_string_t));
+    if(new_ptr != NULL)
+    {
+        new_ptr->buffer = malloc(sizeof(char)*initial_length);
+        new_ptr->capacity = initial_length;
+        new_ptr->length = 0;
+    }
+    return new_ptr;
+}
+
+ptr_string_t* ptr_string_clone(const ptr_string_t * const str)
+{
+    ptr_string_t* new_ptr = ptr_string_new_with_length(str->capacity);
+    if(new_ptr != NULL)
+    {
+        new_ptr->length = str->length;
+        for (int i = 0; i < new_ptr->length; i++)
+        {
+            new_ptr->buffer[i] = str->buffer[i];
+        }
+    }
+    return new_ptr;
+}
