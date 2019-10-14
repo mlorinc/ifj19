e81a17ad07a4becb65dfabbe13784bd968223123
bixorko <peter.vinarcik99@gmail.com>
2019-10-14 22:23:00 +0200

    edited ptr_string.c header added ptr_string_new

--- src/ptr_string.c
+++ src/ptr_string.c
@@ -27,43 +27,51 @@ ptr_string_t ptr_string_new_with_length(const size_t initial_length)
         if(new_ptr->buffer == NULL)
         {
             free(new_ptr);
-            error_exit(ERROR_INTERNAL);
+            return NULL;
         }
-        new_ptr->capacity = initial_length;
-        new_ptr->length = 0;
-    }
-    else {
-        error_exit(ERROR_INTERNAL);
+        new_ptr->length = initial_length;
     }
     return new_ptr;
 }
 
 ptr_string_t ptr_string_clone(const ptr_string_t const str)
 {
-    ptr_string_t new_ptr = ptr_string_new_with_length(str->capacity);
+    ptr_string_t new_ptr = ptr_string_new_with_length(str->length);
     if(new_ptr != NULL)
     {
         new_ptr->length = str->length;
-        for (int i = 0; i < new_ptr->length; i++)
-        {
-            new_ptr->buffer[i] = str->buffer[i];
-        }
-    }
-    else{
-        error_exit(ERROR_INTERNAL);
+        memcpy(new_ptr->buffer, str->buffer, str->length);
     }
     return new_ptr;
 }
 
-ptr_string_t* ptr_string(const char *str)
+char *ptr_string_c_string(ptr_string_t const str)
+{
+    size_t length = str->length + 1;
+    char* stringValue = (char*) malloc(sizeof(char)*length);
+    if(stringValue != NULL)
+    {
+        memcpy(stringValue, str->buffer, length - 1);
+        stringValue[length] = '\0';
+    }
+
+    return stringValue;
+}
+
+ptr_string_t ptr_string(const char *str)
 {
     size_t str_len = strlen(str);
-    ptr_string_t *casted_str = ptr_string_new_with_length(str_len);
+    ptr_string_t casted_str = ptr_string_new_with_length(str_len);
+    if (casted_str == NULL)
+    {
+        // TODO set error flag
+        return NULL;
+    }
 
     // Copies whole string without '\\0'
     memcpy(casted_str->buffer, str, str_len);
     casted_str->length = str_len;
-
+    return casted_str;
 }
 
 ptr_string_t* ptr_string_new()
@@ -72,20 +80,3 @@ ptr_string_t* ptr_string_new()
 
     return new_ptr;
 }
-
-ptr_string_t ptr_string(const char *str)
-{
-    size_t str_len = strlen(str);
-    ptr_string_tcasted_str = ptr_string_new_with_length(str_len);
-    if (casted_str == NULL)
-    {
-        // TODO set error flag
-        return NULL;
-    }
-    
-    // Copies whole string without '\\0'
-    memcpy(casted_str->buffer, str, str_len);
-    casted_str->length = str_len;
-
-}
-
