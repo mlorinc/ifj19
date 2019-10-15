aefe96ccc2c0486b08ae669ae36b500c6fc94050
bixorko <peter.vinarcik99@gmail.com>
2019-10-15 14:32:48 +0200

    added string#insert and #concatenate

--- src/ptr_string.c
+++ src/ptr_string.c
@@ -17,6 +17,7 @@
 
 #include "ptr_string.h"
 #include "error.h"
+#include <assert.h>
 
 ptr_string_t ptr_string_new_with_length(const size_t initial_length)
 {
@@ -83,6 +84,107 @@ ptr_string_t ptr_string(const char *str)
 ptr_string_t* ptr_string_new()
 {
     ptr_string_t* new_ptr = ptr_string_new_with_length(__PTR_STRING_CHUNK);
+    if (new_ptr == NULL){
+        error_exit(ERROR_INTERNAL);
+    }
 
     return new_ptr;
+}
+
+/**
+ * Deletes string from memory. Deleting the same string is considered as safe operation.
+ * @param str string to be deleted
+ * @author Marian Lorinc
+ * @returns bool - true if delete was successfull, false otherwise
+ */
+bool ptr_string_delete(ptr_string_t const str) {
+    if (str == NULL || str->buffer == NULL) {
+        return false;
+    }
+
+    free(str->buffer);
+    str->buffer = NULL;
+    str->length = 0;
+
+    return true;
+}
+
+
+/**
+ * Appeds character at end of str.
+ * @param str source string
+ * @param c character to be appended
+ * @author Marian Lorinc
+ * @returns new string or NULL, when error occurs
+ */
+ptr_string_t ptr_string_append(ptr_string_t const str, const char c) {
+    assert(c != '\0');
+
+    if(str == NULL || str->buffer == NULL) return NULL;
+
+    ptr_string_t newStr = ptr_string_new_with_length(str->length + 1);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    memcpy(newStr->buffer, str->buffer, str->length);
+    newStr->buffer[str->length] = c;
+
+    return newStr;
+}
+
+/**
+ * Returns substring from start to end.
+ * @param str source string
+ * @param start start index (inclusive)
+ * @param end end index (exclusive)
+ * @author Marian Lorinc
+ * @returns new string or NULL, when error occurs
+ */
+ptr_string_t ptr_string_substring(ptr_string_t const str, const size_t start, const size_t end) {
+    if(str == NULL || str->buffer == NULL) return NULL;
+    assert(end > start);
+    assert(str->length > start && str->length <= end);
+
+    size_t length = end - start;
+    ptr_string_t newStr = ptr_string_new_with_length(length);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    memcpy(newStr->buffer, str->buffer + start, length);
+
+    return newStr;
+}
+
+ptr_string_t ptr_string_concat(ptr_string_t const str, ptr_string_t const str_to_concat)
+{
+    ptr_string_t newStr = ptr_string_new_with_length(str->length + str_to_concat->length);
+    memcpy(newStr->buffer, str->buffer, str->length);
+    strcat(newStr->buffer, str_to_concat->buffer);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    return newStr;
+}
+
+ptr_string_t ptr_string_insert(ptr_string_t const str, ptr_string_t const str_to_insert)
+{
+    ptr_string_t newStr = ptr_string_new_with_length(str->length + str_to_insert->length);
+    memcpy(newStr->buffer, str_to_insert->buffer, str_to_insert->length);
+    strcat(newStr->buffer, str->buffer);
+
+    if (newStr == NULL)
+    {
+        return NULL;
+    }
+
+    return newStr;
 }
