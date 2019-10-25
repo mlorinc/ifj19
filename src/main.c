af1970ae94b3b96949af551b07da9a06b6d22b55
Lukas Javorsky <ljavorsk@redhat.com>
2019-10-25 15:35:32 +0200

    Delete the simple main test

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include "ptr_string.h"
-
-int main (void){
-    ptr_string_t str = ptr_string_new_with_length(7);
-    str->buffer = "Ahdjo";
-    printf("%ld\n",ptr_string_find_char_from_index(str, 'o', 0));
-}
