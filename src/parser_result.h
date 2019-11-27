49efc6c2f75c9ed4d86de133c076671550cd9fe1
Marian Lorinc <xlorin01@vutbr.cz>
2019-11-27 13:28:55 +0100

    Add util function to set parser error directly

--- src/parser_result.h
+++ src/parser_result.h
@@ -11,6 +11,7 @@ struct parser_result {
 typedef struct parser_result parser_result_t;
 
 parser_result_t parser_result(ast_t ast);
+parser_result_t parser_error_ptr_string(ast_t ast, ptr_string_t error);
 parser_result_t parser_error(ast_t ast, const char *format, ...);
 bool parser_error_dispose(parser_result_t parser_error);
 
