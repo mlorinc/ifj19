ee02c4de755e6d66505051cc997fba72c82e5126
Marian Lorinc <xlorin01@vutbr.cz>
2019-11-27 13:30:32 +0100

    Fix parser bugs and build whole ast

--- src/parser.h
+++ src/parser.h
@@ -23,18 +23,4 @@ bool accept(parser_t parser, tToken_type token_type);
 
 void parser_return_back(parser_t parser, tToken token);
 
-ast_t factor(parser_t parser);
-
-ast_t term(parser_t parser);
-
-ast_t expression(parser_t parser);
-
-ast_t parse();
-
-ast_t functionDef(parser_t parser, queue_t queue);
-
-ast_t functionParams(parser_t parser, ast_t ast);
-
-void stderr_print(long int line, tToken type, parser_t parser);
-
 #endif
