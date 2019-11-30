545d7b55dce81b0368c3b3d1aa56af51ee919acd
Marian Lorinc <xlorin01@vutbr.cz>
2019-11-30 18:49:56 +0100

    Minor rework of semantic analyser

--- src/semantic_analyser.c
+++ src/semantic_analyser.c
@@ -5,12 +5,14 @@
 #include "symtable.h"
 #include "ptr_string.h"
 
-typedef struct scope
+semantic_result_t semantic_result(ast_t ast, scope_t scope, enum error_codes status)
 {
-    ast_t root;
-    hash_map_t local_table;
-    struct scope *parent_scope;
-} * scope_t;
+    semantic_result_t sem;
+    sem.ast = ast;
+    sem.scope = scope;
+    sem.status = status;
+    return sem;
+}
 
 scope_t new_scope(scope_t parent, ast_t root)
 {
@@ -18,6 +20,13 @@ scope_t new_scope(scope_t parent, ast_t root)
     scope->root = root;
     scope->local_table = hash_map_init();
     scope->parent_scope = parent;
+
+    if (parent == NULL) {
+        scope->root_scope = scope;
+    }
+    else {
+        scope->root_scope = parent->root_scope;
+    }
     return scope;
 }
 
@@ -77,32 +86,46 @@ bool exists_variable_in_scope(scope_t scope, ptr_string_t key)
     return find_scope_with_defined_variable(scope, key) != NULL;
 }
 
-bool set_variable_in_scope(scope_t scope, const char *key, void *value)
+bool set_variable_in_scope(scope_t scope, ptr_string_t key, void *value)
 {
-    return hash_map_put(scope->local_table, key, value);
+    scope_t variable_scope;
+    scope_t func_scope = find_first_node_type_in_scope(scope, FUNCTION_DEFINITION);
+
+    // we are in global scope
+    if (func_scope == NULL) {
+        variable_scope = scope->root_scope;
+    }
+    else {
+        variable_scope = func_scope;
+    }
+
+    char *variable_key = ptr_string_c_string(key);
+    bool result = hash_map_put(variable_scope->local_table, variable_key, value);
+    free(variable_key);
+
+    return result;
 }
 
-void print_undefined_variable_error(ptr_string_t variable_name)
+bool set_function(scope_t scope, ptr_string_t key, void *value)
 {
-    char *variable = ptr_string_c_string(variable_name);
-    fprintf(stderr, "Variable %s is undefined\n", variable);
-    free(variable);
+    return set_variable_in_scope(scope->root_scope, key, value);
 }
 
-scope_t handle_fuction_definition(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_fuction_definition(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     // todo: supports inner function, remove if it is not in ifj assignment
     // get function name
     ptr_string_t function_name = node->data;
 
     // check if it already exists
-    if (exists_variable_in_scope(current_scope, function_name))
+    if (exists_variable_in_scope(current_scope->root_scope, function_name))
     {
         // function redefinition is not allowed
         char *variable = ptr_string_c_string(function_name);
-        fprintf(stderr, "Function %s is already defined in scope\n", variable);
+        ast_t func_node = hash_map_get(current_scope->root_scope->local_table, variable);
+        fprintf(stderr, "Function %s is already defined in scope (line: %d, first defined line: %u)\n", variable, node->line, func_node->line);
         free(variable);
-        return current_scope;
+        return semantic_result(node, current_scope, ERROR_SEM);
     }
     else
     {
@@ -115,11 +138,13 @@ scope_t handle_fuction_definition(scope_t current_scope, ast_t node, deque_t tre
         // extract function body
         ast_t body = array_nodes_get(node->nodes, 1);
 
-        deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, NULL));
+        deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
 
         // make sure types are exact
         assert(parameters->node_type == FUNCTION_PARAMETERS);
         assert(body->node_type == CONSEQUENT);
+
+        enum error_codes status = ERROR_OK;
 
         // argument shadowing is supported, so lets them add to new function scope
         for (size_t i = 0; i < array_nodes_size(parameters->nodes); i++)
@@ -127,19 +152,30 @@ scope_t handle_fuction_definition(scope_t current_scope, ast_t node, deque_t tre
             // add parameters to local variable symbol table
             ast_t arg = array_nodes_get(parameters->nodes, i);
             char *key = ptr_string_c_string(arg->data);
-            hash_map_put(scope->local_table, key, arg);
+
+            // repeating parameter, thats error
+            if (hash_map_has(scope->local_table, key))
+            {
+                status = ERROR_SEM;
+            }
+            else
+            {
+                hash_map_put(scope->local_table, key, arg);
+            }
             free(key);
         }
+
+        set_function(current_scope, function_name, node);
 
         // send body of function to be checked semantically
         deque_push_back(tree_traversing_deque, body);
 
         // return new function scope
-        return scope;
+        return semantic_result(node, scope, status);
     }
 }
 
-scope_t handle_assign(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_assign(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     // extract id from assign
     ast_t id = array_nodes_get(node->nodes, 0);
@@ -149,23 +185,40 @@ scope_t handle_assign(scope_t current_scope, ast_t node, deque_t tree_traversing
     // find scope, where id is defined
     scope_t scope = find_scope_with_defined_variable(current_scope, id->data);
 
+    char *key = ptr_string_c_string(id->data);
+
+    enum error_codes status = ERROR_OK;
+
     // if id is not defined, define it in current scope
     if (scope == NULL)
     {
-        hash_map_put(current_scope->local_table, id->data, NULL);
+        set_variable_in_scope(current_scope, id->data, id);
     }
     else
     {
-        // if it is overwrite it in found scope
-        hash_map_put(scope->local_table, id->data, NULL);
+        // if it is, check if it is not function
+        ast_t variable_node = hash_map_get(scope->local_table, key);
+
+        if (variable_node->node_type == FUNCTION_DEFINITION)
+        {
+            status = ERROR_SEM;
+            fprintf(stderr, "Variable %s cannot have same name as declared function (variable line: %u, function line: %u)\n", key, id->line, variable_node->line);
+        }
+        else
+        {
+            hash_map_put(scope->local_table, key, id);
+        }
     }
+
+    free(key);
+
     // send expression for semantic test
     deque_push_back(tree_traversing_deque, expression);
     // scope did not change
-    return current_scope;
+    return semantic_result(node, current_scope, status);
 }
 
-scope_t handle_while(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_while(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     // extract condition from while
     ast_t condition = array_nodes_get(node->nodes, 0);
@@ -174,7 +227,7 @@ scope_t handle_while(scope_t current_scope, ast_t node, deque_t tree_traversing_
     // extract else clause which is optional
     ast_t else_clause = array_nodes_try_get(node->nodes, 2);
 
-    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, NULL));
+    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
 
     if (else_clause != NULL)
     {
@@ -184,10 +237,10 @@ scope_t handle_while(scope_t current_scope, ast_t node, deque_t tree_traversing_
     deque_push_back(tree_traversing_deque, body);
     deque_push_back(tree_traversing_deque, condition);
 
-    return new_scope(current_scope, node);
+    return semantic_result(node, new_scope(current_scope, node), ERROR_OK);
 }
 
-scope_t handle_if(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_if(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     // extract condition from while
     ast_t condition = array_nodes_get(node->nodes, 0);
@@ -201,29 +254,31 @@ scope_t handle_if(scope_t current_scope, ast_t node, deque_t tree_traversing_deq
         deque_push_back(tree_traversing_deque, alternate_clause);
     }
 
-    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, NULL));
+    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
     deque_push_back(tree_traversing_deque, body);
     deque_push_back(tree_traversing_deque, condition);
 
-    return new_scope(current_scope, node);
+    return semantic_result(node, new_scope(current_scope, node), ERROR_OK);
 }
 
-scope_t handle_else(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_else(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     ast_t body = array_nodes_get(node->nodes, 0);
-    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, NULL));
+    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
     deque_push_back(tree_traversing_deque, body);
-    return new_scope(current_scope, node);
+    return semantic_result(node, new_scope(current_scope, node), ERROR_OK);
 }
 
-scope_t handle_return(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_return(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     // we must ensure, return is in function
     scope_t function_scope = find_first_node_type_in_scope(current_scope, FUNCTION_DEFINITION);
+    enum error_codes status = ERROR_OK;
 
     if (function_scope == NULL)
     {
-        fprintf(stderr, "return is not inside function\n");
+        fprintf(stderr, "return is not inside function (line %u)\n", node->line);
+        status = ERROR_SYNTAX;
     }
 
     ast_t expr = array_nodes_get(node->nodes, 0);
@@ -234,22 +289,24 @@ scope_t handle_return(scope_t current_scope, ast_t node, deque_t tree_traversing
         deque_push_back(tree_traversing_deque, expr);
     }
 
-    return current_scope;
+    return semantic_result(node, current_scope, status);
 }
 
-scope_t handle_break_continue(scope_t current_scope, const char *what)
+semantic_result_t handle_break_continue(scope_t current_scope, ast_t node, const char *what)
 {
     // we must ensure, return is in function
     scope_t while_scope = find_first_node_type_in_scope(current_scope, WHILE);
+    enum error_codes status = ERROR_OK;
 
     if (while_scope == NULL)
     {
-        fprintf(stderr, "%s is not inside while\n", what);
+        fprintf(stderr, "%s is not inside while (line %u)\n", what, node->line);
+        status = ERROR_SYNTAX;
     }
-    return current_scope;
+    return semantic_result(node, current_scope, status);
 }
 
-scope_t handle_consequent(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_consequent(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     array_nodes_t nodes = node->nodes;
     size_t size = array_nodes_size(nodes);
@@ -257,7 +314,7 @@ scope_t handle_consequent(scope_t current_scope, ast_t node, deque_t tree_traver
     // todo: verify if this code is reachable
     if (size == 0)
     {
-        return current_scope;
+        return semantic_result(node, current_scope, ERROR_OK);
     }
 
     for (size_t i = size - 1; i > 0; i--)
@@ -267,30 +324,30 @@ scope_t handle_consequent(scope_t current_scope, ast_t node, deque_t tree_traver
 
     deque_push_back(tree_traversing_deque, array_nodes_get(nodes, 0));
 
-    return current_scope;
+    return semantic_result(node, current_scope, ERROR_OK);
 }
 
 /**
  * Returns error count so far
  */
-scope_t handle_node(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
+semantic_result_t handle_node(scope_t current_scope, ast_t node, deque_t tree_traversing_deque)
 {
     switch (node->node_type)
     {
     case LEAVE_SCOPE:
-        return delete_scope(current_scope);
+        return semantic_result(node, delete_scope(current_scope), ERROR_OK);
 
     case PASS:
-        return current_scope;
+        return semantic_result(node, current_scope, ERROR_OK);
 
     case RETURN:
         // todo: validate expression
         return handle_return(current_scope, node, tree_traversing_deque);
     case CONTINUE:
-        return handle_break_continue(current_scope, "continue");
+        return handle_break_continue(current_scope, node, "continue");
 
     case BREAK:
-        return handle_break_continue(current_scope, "break");
+        return handle_break_continue(current_scope, node, "break");
 
     case FUNCTION_DEFINITION:
         return handle_fuction_definition(current_scope, node, tree_traversing_deque);
@@ -309,33 +366,43 @@ scope_t handle_node(scope_t current_scope, ast_t node, deque_t tree_traversing_d
         return handle_else(current_scope, node, tree_traversing_deque);
 
     case NONE:
-        return current_scope;
+        return semantic_result(node, current_scope, ERROR_OK);
 
     case EXPRESSION:
-        break;
+        return semantic_result(node, current_scope, ERROR_INTERNAL);;
 
     case CONSEQUENT:
         return handle_consequent(current_scope, node, tree_traversing_deque);
 
     default:
-        fprintf(stderr, "Forgot to implement %d\n", node->node_type);
-        break;
+        fprintf(stderr, "Forgot to implement %d on line %u\n", node->node_type, node->line);
+        return semantic_result(node, current_scope, ERROR_INTERNAL);
     }
-
-    return current_scope;
 }
 
-void semantic_analysis(ast_t root)
+semantic_result_t semantic_analysis(ast_t root)
 {
-    scope_t current_scope = new_scope(NULL, root);
+    if (root == NULL) {
+        return semantic_result(NULL, NULL, ERROR_OK);
+    }
 
+    scope_t current_scope = new_scope(NULL, root);
+    
     deque_t tree_traversing_deque = deque_init();
     deque_push_back(tree_traversing_deque, root);
+
+    enum error_codes status = ERROR_OK;
 
     while (deque_size(tree_traversing_deque) > 0)
     {
         ast_t node = deque_pop_back(tree_traversing_deque);
-        current_scope = handle_node(current_scope, node, tree_traversing_deque);
+        semantic_result_t result = handle_node(current_scope, node, tree_traversing_deque);
+        current_scope = result.scope;
+
+        if (status != ERROR_SYNTAX && status != ERROR_INTERNAL)
+        {
+            status = result.status;
+        }
     }
 
     deque_destroy(tree_traversing_deque);
@@ -344,4 +411,5 @@ void semantic_analysis(ast_t root)
     assert(current_scope->root == root);
 
     delete_scope(current_scope);
+    return semantic_result(root, NULL, status);
 }
