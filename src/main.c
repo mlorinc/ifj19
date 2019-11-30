7edabc9b39aa8b134ed041b23de6f0cc8326a3b8
Marian Lorinc <xlorin01@vutbr.cz>
2019-11-30 18:50:22 +0100

    Add semantic check to main

--- src/main.c
+++ src/main.c
@@ -1,15 +1,28 @@
 #include "parser.h"
 #include "parser_result.h"
 #include "parser_ast.h"
+#include "semantic_analyser.h"
 
 int main(void)
 {
     parser_result_t result = parse();
 
-    if (result.ast != NULL) {
-        ast_delete(result.ast);
+    if (result.error_code != ERROR_OK) {
+        if (result.ast != NULL) {
+            ast_delete(result.ast);
+        }
+        return result.error_code;
     }
 
-    return result.error_code;
+    semantic_result_t semantic = semantic_analysis(result.ast);
+
+    if (semantic.status != ERROR_OK) {
+        if (semantic.ast != NULL) {
+            ast_delete(result.ast);
+        }
+        return semantic.status;
+    }
+
+    return ERROR_OK;
 }
 
