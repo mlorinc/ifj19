ce5c271883d16271399151eaca347852f5956f2e
bixorko <peter.vinarcik99@gmail.com>
2019-12-01 11:46:48 +0100

    added simple generator for function definition

---
+++ src/generator.h
@@ -0,0 +1,11 @@
+//
+// Created by bixor on 12/1/19.
+//
+
+#ifndef IFJ19_GENERATOR_H
+#define IFJ19_GENERATOR_H
+
+#include "parser_ast.h"
+#include "semantic_analyser.c"
+
+#endif //IFJ19_GENERATOR_H
