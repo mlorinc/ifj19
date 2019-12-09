ce960486007cb0b3b3e4586a7f987b1929654922
Marian Lorinc <xlorin01@vutbr.cz>
2019-12-09 22:51:57 +0100

    Remove while-else from while scope

--- src/semantic_analyser.c
+++ src/semantic_analyser.c
@@ -139,13 +139,12 @@ semantic_result_t handle_while(scope_t current_scope, ast_t node, deque_t tree_t
     // extract else clause which is optional
     ast_t else_clause = array_nodes_try_get(node->nodes, 2);
 
-    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
-
     if (else_clause != NULL)
     {
         deque_push_back(tree_traversing_deque, else_clause);
     }
 
+    deque_push_back(tree_traversing_deque, ast_node_init(LEAVE_SCOPE, 0, 0, NULL));
     deque_push_back(tree_traversing_deque, body);
     deque_push_back(tree_traversing_deque, condition);
 
