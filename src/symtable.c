68a34da6082d7b068952ad9ec0945d6aeba95e63
Patrik Ondriga <xondri08@vutbr.cz>
2019-12-09 23:56:46 +0100

    Implementation of hash map.

--- src/symtable.c
+++ src/symtable.c
@@ -1,3 +1,162 @@
 #include "symtable.h"
+#include <string.h>
 
+#define BUCKET_SIZE 60
+struct item
+{
+	char* key;
+	void* data;
+	item_t ptrnext;
+};
 
+/**
+ * Generate index for hash map.
+ * @param key item key.
+ * @param index in interval <0,60>
+ */
+int hash_code (char* key) {
+	int retval = 1;
+	int keylen = strlen(key);
+	for ( int i=0; i<keylen; i++ )
+		retval += key[i];
+	return ( retval % BUCKET_SIZE );
+}
+
+/**
+ * Searching for hash map item.
+ * @param hash_map place where I can searching.
+ * @param key searching key.
+ * @return if hash map item was found it will be return, else NULL.
+ */
+void *hash_search(hash_map_t hash_map, const char *key)
+{
+	item_t actual = NULL;
+	for (int i = 0; i < BUCKET_SIZE; i++)
+	{
+		actual = hash_map[i];
+		while (actual != NULL)
+		{
+			if(strcmp(actual->key, key) == 0)
+            {
+				return actual;
+            }
+			actual = actual->ptrnext;
+		}
+	}
+	return NULL;
+}
+
+hash_map_t hash_map_init()
+{
+    hash_map_t new = (hash_map_t) malloc(sizeof(item_t) * BUCKET_SIZE);
+    for(int i=0; i<BUCKET_SIZE; i++0)
+    {
+        new[i] = NULL;
+    }
+    return new;
+}
+
+bool hash_map_put(hash_map_t hash_map, const char *key, void *element)
+{
+    item_t foundItem = hash_search(hash_map, key);
+	if(foundItem != NULL)
+    {
+		foundItem->data = element;
+        return true;
+    }
+	else
+	{
+		int id = hash_code(key);
+		item_t new = (item_t) malloc(sizeof(struct item));
+        if(new == NULL)
+        {
+            return false;
+        }
+		new->data = element;
+		new->key = key;
+		new->ptrnext = hash_map[id];
+		hash_map[id] = new;
+        return true;
+	}
+}
+
+bool hash_map_has(hash_map_t hash_map, const char *key)
+{
+    if(hash_search(hash_map, key) != NULL)
+    {
+        return true;
+    }
+    return false;
+}
+
+void *hash_map_get(hash_map_t hash_map, const char *key)
+{
+    item_t found_item = hash_search(hash_map, key);
+    if(found_item != NULL)
+    {
+        return found_item->data;
+    }
+    return NULL;
+}
+
+bool hash_map_clear(hash_map_t hash_map)
+{
+    for(int i=0; i<BUCKET_SIZE; i++)
+    {
+        if(hash_map[i] != NULL)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void hash_map_destroy(hash_map_t hash_map)
+{
+    item_t actual = NULL;
+	item_t next = NULL;
+	for (int i = 0; i < BUCKET_SIZE; i++)
+	{
+		actual = hash_map[i];
+		while(actual != NULL)
+		{
+			next = actual->ptrnext;
+            free(actual->data);
+            free(actual->key);
+			free(actual);
+			actual = next;
+		}
+		hash_map[i] = NULL;
+	}
+}
+
+void hash_map_delete_item (hash_map_t hash_map, const char* key) {
+	item_t actual = NULL;
+	item_t previous = NULL;
+	for (int i = 0; i < BUCKET_SIZE; i++)
+	{
+		actual = hash_map[i];
+		if(actual != NULL)
+		{
+			if(strcmp(actual->key, key) == 0)
+			{
+				hash_map[i] = actual->ptrnext;
+				free(actual);
+				return;
+			}
+			previous = actual;
+			actual = previous->ptrnext;
+			while(actual != NULL)
+			{
+				if(strcmp(actual->key, key) == 0)
+				{
+					previous->ptrnext = actual->ptrnext;
+					free(actual);
+					return;
+				}
+				previous = actual;
+				actual = previous->ptrnext;
+			}
+		}
+	}
+}
--- src/symtable.h
+++ src/symtable.h
@@ -2,21 +2,63 @@
 #define __H_SYMTABLE
 
 #include <stdbool.h>
+#include "queue.h"
 
-struct hash_map;
-typedef struct hash_map* hash_map_t;
+typedef struct item* item_t;
+typedef item_t** hash_map_t;
 typedef unsigned (*hash_function_t)(const char *key);
 
-#define BUCKET_SIZE 60
-
+/**
+ * Initialization new hash map.
+ * @return pointer on new hash map or NULL on malloc error.
+ */
 hash_map_t hash_map_init();
+
+/**
+ * Insert item.
+ * @param hash_map place where I can insert.
+ * @param key searching key.
+ * @param element inserting item.
+ * @return if operation was successful true, else false.
+ */
 bool hash_map_put(hash_map_t hash_map, const char *key, void *element);
+
+/**
+ * Searching item by key.
+ * @param hash_map place where I can searching.
+ * @param key searching key.
+ * @return if item was found true, else false.
+ */
 bool hash_map_has(hash_map_t hash_map, const char *key);
+
+/**
+ * Searching item by key.
+ * @param hash_map place where I can searching.
+ * @param key searching key.
+ * @return if item was found it will be return, else NULL.
+ */
 void *hash_map_get(hash_map_t hash_map, const char *key);
+
+/**
+ * Find out if hash map is empty.
+ * @param hash_map
+ * @return if it's empty true, else false.
+ */
 bool hash_map_clear(hash_map_t hash_map);
-bool hash_map_destroy(hash_map_t hash_map);
+
+/**
+ * Destroy entire hash map.
+ * @param hash_map for destroy.
+ */
+void hash_map_destroy(hash_map_t hash_map);
+
+/**
+ * Delete item in hash map.
+ * @param hash_map where I can delete item.
+ * @param key item key.
+ */
+void hash_map_delete_item (hash_map_t hash_map, const char* key);
 
 void hash_map_set_hash_function(hash_map_t hash_map, hash_function_t hash_function);
 
-#undef BUCKET_SIZE
 #endif
