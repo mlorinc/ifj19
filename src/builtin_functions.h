53172d41a200dd2f50899405ac562b2f2f306b0e
Lukas Javorsky <ljavorsk@redhat.com>
2019-12-10 19:40:06 +0100

    Initial builtin commit

---
+++ src/builtin_functions.h
@@ -0,0 +1,23 @@
+/**
+ * IFJ2019 @ VUT FIT Brno
+ * 09.11.2019
+ *
+ * Variant 2
+ * Team: 054
+ *
+ * Authors:
+ * Marian Lorinc (xlorin01)
+ * Lukas Javorsky (xjavor20)
+ * Patrik Ondriga (xondri08)
+ * Peter Vinarcik (xvinar00)
+ *
+ * File: scaner.h
+ *
+ */
+
+#ifndef IFJ19_BUILTIN_H
+#define IFJ19_BUILTIN_H
+
+
+
+#endif //IFJ19_SCANER_H
