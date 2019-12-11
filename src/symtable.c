8b0c1939ab6af803e6aa059fb813ccb7254ed922
Marian Lorinc <xlorin01@vutbr.cz>
2019-12-11 22:17:03 +0100

    Add hash map iterator

--- src/symtable.c
+++ src/symtable.c
@@ -2,24 +2,23 @@
 #include <string.h>
 
 #define BUCKET_SIZE 60
-struct item
+struct hash_map
 {
-	char* key;
-	void* data;
-	item_t ptrnext;
+	item_t *buckets;
+	size_t size;
 };
 
 /**
  * Generate index for hash map.
  * @param key item key.
- * @param index in interval <0,60>
  */
-int hash_code (char* key) {
+int hash_code(char *key)
+{
 	int retval = 1;
 	int keylen = strlen(key);
-	for ( int i=0; i<keylen; i++ )
+	for (int i = 0; i < keylen; i++)
 		retval += key[i];
-	return ( retval % BUCKET_SIZE );
+	return (retval % BUCKET_SIZE);
 }
 
 /**
@@ -33,13 +32,13 @@ void *hash_search(hash_map_t hash_map, char *key)
 	item_t actual = NULL;
 	for (int i = 0; i < BUCKET_SIZE; i++)
 	{
-		actual = hash_map[i]; //TODO 100% wokring :kappa:
+		actual = hash_map->buckets[i]; //TODO 100% wokring :kappa:
 		while (actual != NULL)
 		{
-			if(strcmp(actual->key, key) == 0)
-            {
+			if (strcmp(actual->key, key) == 0)
+			{
 				return actual;
-            }
+			}
 			actual = actual->ptrnext;
 		}
 	}
@@ -48,115 +47,122 @@ void *hash_search(hash_map_t hash_map, char *key)
 
 hash_map_t hash_map_init()
 {
-    hash_map_t new = (hash_map_t) malloc(sizeof(item_t) * BUCKET_SIZE);
-    for(int i=0; i<BUCKET_SIZE; i++)
-    {
-        new[i] = NULL;
-    }
-    return new;
+	hash_map_t map = malloc(sizeof(struct hash_map));
+	map->size = 0;
+	map->buckets = malloc(sizeof(item_t) * BUCKET_SIZE);
+
+	for (int i = 0; i < BUCKET_SIZE; i++)
+	{
+		map->buckets[i] = NULL;
+	}
+	return map;
 }
 
 bool hash_map_put(hash_map_t hash_map, char *key, void *element)
 {
-    item_t foundItem = hash_search(hash_map, key);
-	if(foundItem != NULL)
-    {
+	item_t foundItem = hash_search(hash_map, key);
+	if (foundItem != NULL)
+	{
 		foundItem->data = element;
-        return true;
-    }
+		return true;
+	}
 	else
 	{
 		int id = hash_code(key);
-		item_t new = (item_t) malloc(sizeof(struct item));
-        if(new == NULL)
-        {
-            return false;
-        }
+		item_t new = (item_t)malloc(sizeof(struct item));
+		if (new == NULL)
+		{
+			return false;
+		}
 		new->data = element;
-		
+
 		size_t length = strlen(key) + 1;
 		new->key = malloc(length);
 		memcpy(new->key, key, length);
 
-
-		new->ptrnext = hash_map[id];
-        hash_map[id] = new;
-        return true;
+		new->ptrnext = hash_map->buckets[id];
+		hash_map->buckets[id] = new;
+		hash_map->size++;
+		return true;
 	}
 }
 
 bool hash_map_has(hash_map_t hash_map, char *key)
 {
-    if(hash_search(hash_map, key) != NULL)
-    {
-        return true;
-    }
-    return false;
+	if (hash_search(hash_map, key) != NULL)
+	{
+		return true;
+	}
+	return false;
 }
 
 void *hash_map_get(hash_map_t hash_map, char *key)
 {
-    item_t found_item = hash_search(hash_map, key);
-    if(found_item != NULL)
-    {
-        return found_item->data;
-    }
-    return NULL;
+	item_t found_item = hash_search(hash_map, key);
+	if (found_item != NULL)
+	{
+		return found_item->data;
+	}
+	return NULL;
 }
 
 bool hash_map_clear(hash_map_t hash_map)
 {
-    for(int i=0; i<BUCKET_SIZE; i++)
-    {
-        if(hash_map[i] != NULL)
-        {
-            return false;
-        }
-    }
-    return true;
+	for (int i = 0; i < BUCKET_SIZE; i++)
+	{
+		if (hash_map->buckets[i] != NULL)
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 void hash_map_destroy(hash_map_t hash_map)
 {
-    item_t actual = NULL;
+	item_t actual = NULL;
 	item_t next = NULL;
 	for (int i = 0; i < BUCKET_SIZE; i++)
 	{
-		actual = hash_map[i];
-		while(actual != NULL)
+		actual = hash_map->buckets[i];
+		while (actual != NULL)
 		{
 			next = actual->ptrnext;
-            // free(actual->data);
-            free(actual->key);
+			// free(actual->data);
+			free(actual->key);
 			free(actual);
 			actual = next;
 		}
-		hash_map[i] = NULL;
+		hash_map->buckets[i] = NULL;
 	}
+	free(hash_map);
 }
 
-void hash_map_delete_item (hash_map_t hash_map, char* key) {
+void hash_map_delete_item(hash_map_t hash_map, char *key)
+{
 	item_t actual = NULL;
 	item_t previous = NULL;
 	for (int i = 0; i < BUCKET_SIZE; i++)
 	{
-		actual = hash_map[i];
-		if(actual != NULL)
+		actual = hash_map->buckets[i];
+		if (actual != NULL)
 		{
-			if(strcmp(actual->key, key) == 0)
+			if (strcmp(actual->key, key) == 0)
 			{
-				hash_map[i] = actual->ptrnext;
+				hash_map->buckets[i] = actual->ptrnext;
 				free(actual);
+				hash_map->size--;
 				return;
 			}
 			previous = actual;
 			actual = previous->ptrnext;
-			while(actual != NULL)
+			while (actual != NULL)
 			{
-				if(strcmp(actual->key, key) == 0)
+				if (strcmp(actual->key, key) == 0)
 				{
 					previous->ptrnext = actual->ptrnext;
 					free(actual);
+					hash_map->size--;
 					return;
 				}
 				previous = actual;
@@ -164,4 +170,51 @@ void hash_map_delete_item (hash_map_t hash_map, char* key) {
 			}
 		}
 	}
+}
+
+hash_map_iterator_t hash_map_begin(hash_map_t map)
+{
+	hash_map_iterator_t ite;
+	ite.map = map;
+
+	for (size_t i = 0; i < BUCKET_SIZE; i++)
+	{
+		item_t bucket = map->buckets[i];
+
+		if (bucket != NULL) {
+			ite.bucket_index = i;
+			ite.current = bucket;
+			return ite;
+		}
+	}
+	
+	ite.bucket_index = 0;
+	ite.current = NULL;
+	return ite;
+}
+
+hash_map_iterator_t hash_map_iterator_next(hash_map_iterator_t ite)
+{
+	if (ite.current->ptrnext) {
+		ite.current = ite.current->ptrnext;
+		return ite;
+	}
+	
+	for (size_t i = ite.bucket_index + 1; i < BUCKET_SIZE; i++)
+	{
+		item_t bucket = ite.map->buckets[i];
+
+		if (bucket != NULL) {
+			ite.bucket_index = i;
+			ite.current = bucket;
+			return ite;
+		}
+	}
+
+	ite.current = NULL;
+	return ite;
+}
+
+void *hash_map_iterator_value(hash_map_iterator_t ite) {
+	return ite.current;
 }
--- src/symtable.h
+++ src/symtable.h
@@ -4,9 +4,25 @@
 #include <stdbool.h>
 #include "queue.h"
 
+struct item
+{
+	char *key;
+	void *data;
+	struct item* ptrnext;
+};
+
 typedef struct item* item_t;
-typedef item_t* hash_map_t;
+typedef struct hash_map *hash_map_t;
 typedef unsigned (*hash_function_t)(char *key);
+
+struct hash_map_iterator
+{
+	size_t bucket_index;
+	item_t current;
+	hash_map_t map;
+};
+
+typedef struct hash_map_iterator hash_map_iterator_t;
 
 /**
  * Initialization new hash map.
@@ -53,6 +69,12 @@ bool hash_map_clear(hash_map_t hash_map);
 void hash_map_destroy(hash_map_t hash_map);
 
 /**
+ * Returns size of hash map
+ * @param size of hash map
+ */
+size_t hash_map_size(hash_map_t hash_map);
+
+/**
  * Delete item in hash map.
  * @param hash_map where I can delete item.
  * @param key item key.
@@ -60,5 +82,11 @@ void hash_map_destroy(hash_map_t hash_map);
 void hash_map_delete_item (hash_map_t hash_map, char* key);
 
 void hash_map_set_hash_function(hash_map_t hash_map, hash_function_t hash_function);
+
+hash_map_iterator_t hash_map_begin(hash_map_t map);
+
+hash_map_iterator_t hash_map_iterator_next(hash_map_iterator_t ite);
+
+void *hash_map_iterator_value(hash_map_iterator_t ite);
 
 #endif
