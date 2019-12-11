a15b8a4a0cdb80850cc5f032200c696e7a5a3670
Patrik Ondriga <xondri08@vutbr.cz>
2019-12-11 01:14:24 +0100

    Add function substr.

--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -18,7 +18,7 @@
 #include <stdio.h>
 #include "builtin_functions.h"
 
-void builtin_function()
+void inputs()
 {
     printf(
         "LABEL inputs\n"
@@ -28,6 +28,10 @@ void builtin_function()
         "POPFRAME\n"
         "RETURN\n\n"
         );
+}
+
+void inputi()
+{
     printf(
         "LABEL inputi\n"
         "PUSHFRAME\n"
@@ -36,6 +40,10 @@ void builtin_function()
         "POPFRAME\n"
         "RETURN\n\n"
         );
+}
+
+void inputf()
+{
     printf(
         "LABEL inputf\n"
         "PUSHFRAME\n"
@@ -44,6 +52,10 @@ void builtin_function()
         "POPFRAME\n"
         "RETURN\n\n"
         );
+}
+
+void len()
+{
     printf(
         "LABEL len\n"
         "PUSHFRAME\n"
@@ -54,5 +66,66 @@ void builtin_function()
         "POPFRAME\n"
         "RETURN\n\n"
         );
+}
+
+void substr()
+{
+    printf(
+        "LABEL substr\n"
+        "PUSHFRAME\n"
+        "DEFVAR LF@%retval\n"
+        "MOVE LF@%retval nil@nil\n"
+        "DEFVAR LF@s\n"
+        "MOVE LF@s LF@%1\n"
+        "DEFVAR LF@i\n"
+        "MOVE LF@i LF@%2\n"
+        "DEFVAR LF@n\n"
+        "MOVE LF@n LF@%3\n"
+        "DEFVAR LF@jump\n"
+        "DEFVAR LF@lenstring\n"
+        "STRLEN LF@lenstring LF@s\n"
+        "LT LF@jump LF@i int@0\n"
+        "JUMPIFEQ badi LF@jump bool@true\n"
+        "GT LF@jump LF@lenstring LF@i\n"
+        "JUMPIFNEQ badi LF@jump bool@true\n"
+        "LT LF@jump LF@n int@0\n"
+        "JUMPIFEQ badi LF@jump bool@true\n"
+        "DEFVAR LF@lenstringmini\n"
+        "SUB LF@lenstringmini LF@lenstring LF@i\n"
+        "GT LF@jump LF@n LF@lenstringmini\n"
+        "JUMPIFEQ lenlessn LF@jump bool@true\n"
+        "JUMP okn\n"
+        "LABEL lenlessn\n"
+        "MOVE LF@n LF@lenstringmini\n"
+        "LABEL okn\n"
+        "DEFVAR LF@char\n"
+        "DEFVAR LF@tmp\n"
+        "EQ LF@jump LF@n int@0\n"
+        "JUMPIFEQ substrend LF@jump bool@true\n"
+        "GETCHAR LF@char LF@s LF@i\n"
+        "MOVE LF@%retval LF@char\n"
+        "ADD LF@tmp LF@i int@1\n"
+        "MOVE LF@i LF@tmp\n"
+        "SUB LF@tmp LF@n int@1\n"
+        "MOVE LF@n LF@tmp\n"
+        "EQ LF@jump LF@n int@0\n"
+        "JUMPIFEQ substrend LF@jump bool@true\n"
+        "LABEL substrwhile\n"
+        "GETCHAR LF@char LF@s LF@i\n"
+        "CONCAT LF@tmp LF@%retval LF@char\n"
+        "MOVE LF@%retval LF@tmp\n"
+        "ADD LF@tmp LF@i int@1\n"
+        "MOVE LF@i LF@tmp\n"
+        "SUB LF@tmp LF@n int@1\n"
+        "MOVE LF@n LF@tmp\n"
+        "EQ LF@jump LF@n int@0\n"
+        "JUMPIFNEQ substrwhile LF@jump bool@true\n"
+        "JUMP substrend\n"
+        "LABEL badi\n"
+        "MOVE LF@%retval nil@nil\n"
+        "LABEL substrend\n"
+        "POPFRAME\n"
+        "RETURN\n"
+        )
 }
 
